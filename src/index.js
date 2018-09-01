b59ef92bc56ab1589d0a2da67e8ad79468dc45c8
xuejian.xu <jearbilove@yeah.net>
2018-09-01 08:34:32 +0800

    update

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 import App from '@src/container/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
+import './style/common.css'
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();

23fb3248d5d368cbc2f346b5ccea367e4f2316f5
xuejian.xu <jearbilove@yeah.net>
2018-12-13 17:17:41 +0800

    update

--- src/container/People/Client.jsx
+++ src/container/People/Client.jsx
@@ -2,34 +2,91 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 // modules
-import { Table, Modal, Row, Col, Button, notification } from 'antd'
+import { Table, Modal, Row, Col, Button, Form } from 'antd'
 import columns from './clientColumns'
 
+@Form.create()
 @connect(state => ({
     Client: state.Client,
 }))
 class Client extends React.Component {
+    state = {
+        clientId: '',
+        agentId: '',
+        groupId: '',
+    }
     componentDidMount() {
-        this.props.dispatch({
-            type: 'Client/fetchClientsAsync',
-            payload: {
-                pageNo: 0,
-            },
+        this.fetch({
+            pageNo: 0,
         })
     }
-    onPagination = () => {
+    fetch = payload => {
         this.props.dispatch({
             type: 'Client/fetchClientsAsync',
-            payload: {
-                pageNo: next - 1,
-            },
+            payload,
+        })
+    }
+    onPagination = next => {
+        this.fetch({
+            pageNo: next - 1,
+        })
+    }
+    onQuery = () => {
+        const { pageNo } = this.props
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                this.fetch({
+                    pageNo,
+                    params: values,
+                })
+                this.props.form.resetFields()
+            }
         })
     }
     render() {
         const { Client } = this.props
         const { clients, pageNo, pageSize, total } = Client
+        const { clientId, agentId, groupId } = this.state
+        const formItemLayout = {
+            labelCol: {
+                span: 6,
+            },
+            wrapperCol: {
+                span: 16,
+            },
+        }
         return (
             <div className="page-client">
+                <Row>
+                    <Col span={8}>
+                        <FormItem {...formItemLayout} label="客户ID">
+                            {getFieldDecorator('clientId', {
+                                initialValue: clientId,
+                            })(<Input placeholder="请输入客户ID" />)}
+                        </FormItem>
+                    </Col>
+                    <Col span={8}>
+                        <FormItem {...formItemLayout} label="代理人ID">
+                            {getFieldDecorator('agentId', {
+                                initialValue: agentId,
+                            })(<Input placeholder="请输入代理人ID" />)}
+                        </FormItem>
+                    </Col>
+                    <Col span={8}>
+                        <FormItem {...formItemLayout} label="分组ID">
+                            {getFieldDecorator('groupId', {
+                                initialValue: groupId,
+                            })(<Input placeholder="请输入分组ID" />)}
+                        </FormItem>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span={24} style={{ textAlign: 'right' }}>
+                        <Button type="primary" onClick={this.onQuery}>
+                            查询
+                        </Button>
+                    </Col>
+                </Row>
                 <Table
                     style={{ marginTop: 20 }}
                     rowSelection={{

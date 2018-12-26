c8ffd23ae7958e3a4d7f1b8d0829bf2b1b4dbe39
xuejian.xu <jearbilove@yeah.net>
2018-12-26 22:20:15 +0800

    update

--- src/container/People/Agent/Agent.jsx
+++ src/container/People/Agent/Agent.jsx
@@ -11,9 +11,16 @@ class Agent extends React.Component {
     state = {
         currentAgentIdx: 0,
         groups: [],
+        groupModalMode: 'add',
         addGroupModalVisible: false,
         groupNoValue: '',
         groupNameValue: '',
+        agentModalMode: 'add',
+        addAgentModalVisible: false,
+        agentNoValue: '',
+        agentNameValue: '',
+        currentAgentId: '',
+        currentGroupId: '',
     }
     componentDidMount() {
         this.fetch()
@@ -50,12 +57,49 @@ class Agent extends React.Component {
                         <div key={agent.id} className={cls} onClick={this.onAgentItemClick.bind(this, agent, idx)}>
                             <div className="name">{agent.clientAgentName}</div>
                             <div className="button">
-                                <Button type="primary" size="small" ghost>
+                                <Button
+                                    type="primary"
+                                    size="small"
+                                    ghost
+                                    onClick={() => {
+                                        this.setState({
+                                            agentModalMode: 'edit',
+                                            addAgentModalVisible: true,
+                                            agentNameValue: agent.clientAgentName,
+                                            agentNoValue: agent.clientAgentNo,
+                                            currentAgentId: agent.id,
+                                        })
+                                    }}
+                                >
                                     编辑
                                 </Button>
                             </div>
                             <div className="button">
-                                <Button type="danger" size="small" ghost>
+                                <Button
+                                    type="danger"
+                                    size="small"
+                                    ghost
+                                    onClick={() => {
+                                        Modal.confirm({
+                                            title: '确定要删除吗？',
+                                            onOk: () => {
+                                                this.props.dispatch({
+                                                    type: 'Agent/deleteAgentAsync',
+                                                    payload: {
+                                                        id: agent.id,
+                                                        callback: () => {
+                                                            this.fetch()
+                                                            notification.info({
+                                                                message: '提示',
+                                                                description: '删除成功',
+                                                            })
+                                                        },
+                                                    },
+                                                })
+                                            },
+                                        })
+                                    }}
+                                >
                                     删除
                                 </Button>
                             </div>
@@ -65,7 +109,12 @@ class Agent extends React.Component {
             </div>
         )
     }
-    onAgentAdd = () => {}
+    onAgentAdd = () => {
+        this.setState({
+            agentModalMode: 'add',
+            addAgentModalVisible: true,
+        })
+    }
     onGroupAdd = () => {
         const { agents } = this.props
 
@@ -76,6 +125,7 @@ class Agent extends React.Component {
             })
         } else {
             this.setState({
+                groupModalMode: 'edit',
                 addGroupModalVisible: true,
             })
         }
@@ -90,12 +140,49 @@ class Agent extends React.Component {
                         <div key={group.id} className="item" onClick={this.onGroupItemClick.bind(this, group)}>
                             <div className="name">{group.clientGroupName}</div>
                             <div className="button">
-                                <Button type="primary" size="small" ghost>
+                                <Button
+                                    type="primary"
+                                    size="small"
+                                    ghost
+                                    onClick={() => {
+                                        this.setState({
+                                            groupModalMode: 'edit',
+                                            addGroupModalVisible: true,
+                                            groupNameValue: group.clientGroupName,
+                                            groupNoValue: group.clientGroupNo,
+                                            currentGroupId: group.id,
+                                        })
+                                    }}
+                                >
                                     编辑
                                 </Button>
                             </div>
                             <div className="button">
-                                <Button type="danger" size="small" ghost>
+                                <Button
+                                    type="danger"
+                                    size="small"
+                                    ghost
+                                    onClick={() => {
+                                        Modal.confirm({
+                                            title: '确定要删除吗？',
+                                            onOk: () => {
+                                                this.props.dispatch({
+                                                    type: 'Agent/deleteGroupAsync',
+                                                    payload: {
+                                                        id: group.id,
+                                                        callback: () => {
+                                                            this.fetch()
+                                                            notification.info({
+                                                                message: '提示',
+                                                                description: '删除成功',
+                                                            })
+                                                        },
+                                                    },
+                                                })
+                                            },
+                                        })
+                                    }}
+                                >
                                     删除
                                 </Button>
                             </div>
@@ -129,6 +216,63 @@ class Agent extends React.Component {
                     </div>
                 </div>
                 <Modal
+                    visible={this.state.addAgentModalVisible}
+                    title={'添加代理人'}
+                    okText={'确认'}
+                    cancelText={'取消'}
+                    destroyOnClose
+                    onOk={() => {
+                        const isAdding = this.state.agentModalMode === 'add'
+
+                        this.props.dispatch({
+                            type: isAdding ? 'Agent/addAgentAsync' : 'Agent/updateAgentAsync',
+                            payload: {
+                                id: this.state.currentAgentId,
+                                params: {
+                                    clientAgentNo: this.state.agentNoValue,
+                                    clientAgentName: this.state.agentNameValue,
+                                },
+                                callback: () => {
+                                    notification.info({
+                                        message: '提示',
+                                        description: isAdding ? '添加' : '更新' + '代理人成功',
+                                    })
+                                    this.fetch()
+                                    this.setState({
+                                        addAgentModalVisible: false,
+                                        agentNameValue: '',
+                                        agentNoValue: '',
+                                    })
+                                },
+                            },
+                        })
+                    }}
+                    onCancel={() => {
+                        this.setState({
+                            addAgentModalVisible: false,
+                        })
+                    }}
+                >
+                    <React.Fragment>
+                        <Form.Item label="代理人编号">
+                            <Input
+                                value={this.state.agentNoValue}
+                                onChange={e => {
+                                    this.setState({ agentNoValue: e.target.value })
+                                }}
+                            />
+                        </Form.Item>
+                        <Form.Item label="代理人名称">
+                            <Input
+                                value={this.state.agentNameValue}
+                                onChange={e => {
+                                    this.setState({ agentNameValue: e.target.value })
+                                }}
+                            />
+                        </Form.Item>
+                    </React.Fragment>
+                </Modal>
+                <Modal
                     visible={this.state.addGroupModalVisible}
                     title={'添加分组'}
                     okText={'确认'}
@@ -136,11 +280,13 @@ class Agent extends React.Component {
                     destroyOnClose
                     onOk={() => {
                         const { agents } = this.props
-                        const { currentAgentIdx } = this.state
+                        const { currentAgentIdx, groupModalMode, currentGroupId } = this.state
+                        const isAdding = groupModalMode === 'add'
 
                         this.props.dispatch({
-                            type: 'Agent/addGroupAsync',
+                            type: isAdding ? 'Agent/addGroupAsync' : 'Agent/updateGroupAsync',
                             payload: {
+                                id: currentGroupId,
                                 params: {
                                     clientAgentId: agents[currentAgentIdx].id,
                                     clientGroupNo: this.state.groupNoValue,
@@ -149,11 +295,13 @@ class Agent extends React.Component {
                                 callback: () => {
                                     notification.info({
                                         message: '提示',
-                                        description: '添加分组成功',
+                                        description: isAdding ? '添加' : '更新' + '分组成功',
                                     })
                                     this.fetch()
                                     this.setState({
-                                        addGroupModalVisible: false
+                                        addGroupModalVisible: false,
+                                        groupNameValue: '',
+                                        groupNoValue: '',
                                     })
                                 },
                             },
--- src/models/Agent.js
+++ src/models/Agent.js
@@ -15,27 +15,62 @@ const Agent = {
         },
     },
     effects: dispatch => ({
-        async fetchAgentAsync({ callback }){
+        async fetchAgentAsync({ callback }) {
             const res = await ApiService.get(`/api/base/clientAgent/all`)
 
-            if( res.code === 200){
+            if (res.code === 200) {
                 dispatch({
                     type: 'Agent/updateAgent',
                     payload: {
                         agents: res.data,
-                    }
+                    },
                 })
                 callback && callback(res.data)
             }
         },
-        async addGroupAsync({ params, callback }){
+        async addGroupAsync({ params, callback }) {
             const res = await ApiService.post(`/api/clientGroup/add`, params)
 
             if (res.code === 200) {
                 callback && callback()
             }
-        }
-    })
+        },
+        async deleteGroupAsync({ id, callback }) {
+            const res = await ApiService.delete(`/api/clientGroup/delete/${id}`)
+
+            if (res.code === 200) {
+                callback && callback()
+            }
+        },
+        async updateGroupAsync({ id, params, callback }) {
+            const res = await ApiService.put(`/api/clientGroup/update/${id}`, params)
+
+            if (res.code === 200) {
+                callback && callback()
+            }
+        },
+        async addAgentAsync({ params, callback }) {
+            const res = await ApiService.post(`/api/clientAgent/add`, params)
+
+            if (res.code === 200) {
+                callback && callback()
+            }
+        },
+        async deleteAgentAsync({ id, callback }) {
+            const res = await ApiService.delete(`/api/clientAgent/delete/${id}`)
+
+            if (res.code === 200) {
+                callback && callback()
+            }
+        },
+        async updateAgentAsync({ id, params, callback }) {
+            const res = await ApiService.put(`/api/clientAgent/update/${id}`, params)
+
+            if (res.code === 200) {
+                callback && callback()
+            }
+        },
+    }),
 }
 
 export default Agent
